a022aaa576fdd7f3bd84b0d75e577774d3d59d9a
anshulsingla114 <anshulsfd@gmail.com>
2024-06-18 16:38:18 +0530

    second commit

--- dags/youtubeapietl.py
+++ dags/youtubeapietl.py
@@ -18,7 +18,7 @@ def extract_data():
         # Make API request to retrieve comments from a specific video
         request = youtube.commentThreads().list(
             part='snippet',
-            videoId='pg19Z8LL06w',
+            videoId='lpj8YgrlkTk',
             maxResults=100
         )
         response = request.execute()
@@ -77,7 +77,7 @@ def load_to_csv(ti):
             raise FileNotFoundError(f"Directory {output_dir} does not exist after attempt to create it")
 
         # Save the transformed data to a CSV file
-        output_path = os.path.join(output_dir, 'youtube_comments2.csv')
+        output_path = os.path.join(output_dir, 'youtube_comments3.csv')
         logging.info(f"Attempting to save CSV file at: {output_path}")
         transformed_df.to_csv(output_path, index=False)
         logging.info(f"CSV file saved successfully at: {output_path}")
